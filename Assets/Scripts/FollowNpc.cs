0c98268b89747c7a6cbe8d3ddbb8e24f06e00b2d
Vasanth Mohan <vmohan7@stanford.edu>
2017-11-20 16:54:30 -0800

    Inital Commit

---
+++ Assets/Scripts/FollowNpc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowNpc : MonoBehaviour {
+
+	public GameObject attractor;
+
+	public float speed;
+	private Vector3 attractionPoint;
+
+	// Use this for initialization
+	void Start()
+	{
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		attractionPoint = attractor.transform.position; //Update our attraction point every frame
+		this.GetComponent<Rigidbody>().AddForce((attractionPoint - transform.position).normalized * speed); //Add force towards the target AKA gravity
+	}
+
+}
---
+++ Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointMovement : MonoBehaviour {
+	
+	public GameObject player;
+
+
+	public float height = 2;
+	public bool teleport = true;
+
+	public float maxMoveDistance = 10;
+	private bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move(GameObject waypoint) {
+		if (!teleport) {
+			iTween.MoveTo (player, 
+				iTween.Hash (
+					"position", new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + height / 2, waypoint.transform.position.z), 
+					"time", .2F, 
+					"easetype", "linear"
+				)
+			);
+		} else {
+			player.transform.position = new Vector3 (waypoint.transform.position.x, 
+                waypoint.transform.position.y + height / 2, 
+                waypoint.transform.position.z);
+		}
+	}
+
+}
+
